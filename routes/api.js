8eae754521eca6e607f712188744abc74bb4bbcb
El Rafa <rafaelrosas19@gmail.com>
2021-02-17 22:11:12 -0600

    Added catch err to get request

--- routes/api.js
+++ routes/api.js
@@ -3,9 +3,13 @@ const router = require("express").Router();
 const Workout = require("../models/workout.js");
 
 router.get("/api/workouts", (req, res) => {
-  Workout.find({}).then((workout) => {
-    res.json(workout);
-  });
+  Workout.find({})
+    .then((workout) => {
+      res.json(workout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
 });
 
 router.put("/api/workouts/:id", (req, res) => {
