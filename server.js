1c3de11de932e560cfc30bb1e28cf1617683d235
El Rafa <rafaelrosas19@gmail.com>
2021-02-19 16:30:43 -0600

    Getting rid of the UseUnifiedTopology error from mongo

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ app.use(express.static("public"));
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
 });
 
 app.use(require("./routes/api-routes.js"));
